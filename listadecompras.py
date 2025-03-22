cc927d95a0eab1533a07aa29fa79ed237696c97b
bruno2074 <146577585+bruno2074@users.noreply.github.com>
2025-03-22 14:32:49 -0300

    Update listadecompras.py

--- listadecompras.py
+++ listadecompras.py
@@ -18,23 +18,30 @@ while True:
         os.system('clear')
         valor = input('Valor: ')
         lista.append(valor)
+
+    # recebe opcao 
     elif opcao == 'a':
+        # pede índice da lista para apagar
         indice_str = input(
             'Escolha o índice para apagar: '
         )
-
+        # abre um bloco pra verificação de erros em relação ao índice [a]  
         try:
             indice = int(indice_str)
             del lista[indice]
+        # Error caso o número seja float
         except ValueError:
             print('Por favor digite número int.')
+        # Error caso índice não estiver na lista
         except IndexError:
             print('Índice não existe na lista')
+        # Error caso não saiba a definição do erro
         except Exception:
             print('Erro desconhecido')
+    # Recebe opção 
     elif opcao == 'l':
         os.system('clear')
-
+        # Caso a lista tenha 0 índices, apresenta o print "Nada para listar"
         if len(lista) == 0:
             print('Nada para listar')
 
